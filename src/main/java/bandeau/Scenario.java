362f31a0f4f3e35b59e0b6e5020d4b68144c6e4b
Medaminesagh1 <147838684+Medaminesagh1@users.noreply.github.com>
2024-10-09 10:24:15 +0200

    finTp

--- src/main/java/bandeau/Scenario.java
+++ src/main/java/bandeau/Scenario.java
@@ -1,6 +1,11 @@
 package bandeau;
+
 import java.util.List;
 import java.util.LinkedList;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Classe utilitaire pour représenter la classe-association UML
@@ -15,22 +20,29 @@ class ScenarioElement {
         repeats = r;
     }
 }
+
 /**
- * Un scenario mémorise une liste d'effets, et le nombre de repetitions pour chaque effet
+ * Un scenario mémorise une liste d'effets, et le nombre de repetitions pour
+ * chaque effet
  * Un scenario sait se jouer sur un bandeau.
  */
 public class Scenario {
 
-    private final List<ScenarioElement> myElements = new LinkedList<>();
-
+    private final List<ScenarioElement> myElements = new CopyOnWriteArrayList<>();
+    private final ReadWriteLock verrouScenario = new ReentrantReadWriteLock();
     /**
      * Ajouter un effect au scenario.
      *
-     * @param e l'effet à ajouter
+     * @param e       l'effet à ajouter
      * @param repeats le nombre de répétitions pour cet effet
      */
     public void addEffect(Effect e, int repeats) {
-        myElements.add(new ScenarioElement(e, repeats));
+        verrouScenario.writeLock().lock();
+        try {
+            myElements.add(new ScenarioElement(e, repeats));
+        } finally {
+            verrouScenario.writeLock().unlock();
+        }
     }
 
     /**
@@ -39,10 +51,19 @@ public class Scenario {
      * @param b le bandeau ou s'afficher.
      */
     public void playOn(Bandeau b) {
-        for (ScenarioElement element : myElements) {
-            for (int repeats = 0; repeats < element.repeats; repeats++) {
-                element.effect.playOn(b);
+        new Thread(() -> {
+            synchronized (b) {
+                verrouScenario.readLock().lock();
+                try {
+                    for (ScenarioElement element : myElements) {
+                        for (int repeats = 0; repeats < element.repeats; repeats++) {
+                            element.effect.playOn(b);
+                        }
+                    }
+                } finally {
+                    verrouScenario.readLock().unlock();
+                }
             }
-        }
+        }).start();
     }
 }
